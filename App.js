5286355eb8a828ca0a5ac889569d20b3cda1d8cf
karoldavid <k.zysk@zoho.com>
2018-01-18 16:23:51 +0100

    feat: hook up store

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { Provider } from "react-redux";
+import store from "./src/store";
+import { View } from "react-native";
 import { MainNavigator } from "./src/utils/navigation";
 import { styles } from "./src/utils/styles";
 
 export default class App extends Component {
   render() {
     return (
-      <View style={{ flex: 1 }}>
-        <MainNavigator />
-      </View>
+      <Provider store={store}>
+        <View style={{ flex: 1 }}>
+          <MainNavigator />
+        </View>
+      </Provider>
     );
   }
 }
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import DecksReducer from "./DecksReducer";
+
+export default combineReducers({
+	decks: DecksReducer
+});
