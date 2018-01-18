4a0fc6af8633f45f5a7774ef7f578af05c850738
karoldavid <k.zysk@zoho.com>
2018-01-18 18:10:43 +0100

    refactor: load font async in top level component

--- App.js
+++ App.js
@@ -4,15 +4,36 @@ import store from "./src/store";
 import { View } from "react-native";
 import { MainNavigator } from "./src/utils/navigation";
 import { styles } from "./src/utils/styles";
+import { AppLoading, Font } from "expo";
 
 export default class App extends Component {
+  state = {
+    fontLoaded: false
+  };
+
+  componentWillMount() {
+    this._loadFontsAsync();
+  }
+
+  async _loadFontsAsync() {
+    await Font.loadAsync({
+      "Lato-Regular": require("./src/assets/fonts/Lato-Regular.ttf")
+    });
+
+    this.setState({ fontLoaded: true });
+  }
+
   render() {
-    return (
-      <Provider store={store}>
-        <View style={{ flex: 1 }}>
-          <MainNavigator />
-        </View>
-      </Provider>
-    );
+    if (this.state.fontLoaded) {
+      return (
+        <Provider store={store}>
+          <View style={{ flex: 1 }}>
+            <MainNavigator />
+          </View>
+        </Provider>
+      );
+    } else {
+      return <AppLoading />;
+    }
   }
 }
--- src/screens/DeckScreen.js
+++ src/screens/DeckScreen.js
@@ -3,44 +3,29 @@ import { connect } from "react-redux";
 import { Text, View } from "react-native";
 import { Button, Card } from "react-native-elements";
 import { styles } from "../utils/styles";
-import { Font } from "expo";
 
 class DeckScreen extends Component {
-	state = {
-		fontLoaded: false
-	};
-
-	async componentDidMount() {
-		await Font.loadAsync({
-			"Lato-Regular": require("../assets/fonts/Lato-Regular.ttf")
-		});
-
-		this.setState({ fontLoaded: true });
-	}
-	
 	render() {
 		const { title, questions } = this.props.selected;
 		return (
 			<View style={styles.container}>
-				{this.state.fontLoaded ? (
-					<Card title={title} image={require("../img/react.png")}>
-						<Text style={{ marginBottom: 10, textAlign: "center" }}>
-							{`${questions.length} Questions`}
-						</Text>
-						<Button
-							icon={{ name: "code" }}
-							backgroundColor="#03A9F4"
-							fontFamily="Lato-Regular"
-							buttonStyle={{
-								borderRadius: 0,
-								marginLeft: 0,
-								marginRight: 0,
-								marginBottom: 0
-							}}
-							title="Start Quiz"
-						/>
-					</Card>
-				) : null}
+				<Card title={title} image={require("../img/react.png")}>
+					<Text style={{ marginBottom: 10, textAlign: "center" }}>
+						{`${questions.length} Questions`}
+					</Text>
+					<Button
+						icon={{ name: "code" }}
+						backgroundColor="#03A9F4"
+						fontFamily="Lato-Regular"
+						buttonStyle={{
+							borderRadius: 0,
+							marginLeft: 0,
+							marginRight: 0,
+							marginBottom: 0
+						}}
+						title="Start Quiz"
+					/>
+				</Card>
 			</View>
 		);
 	}
