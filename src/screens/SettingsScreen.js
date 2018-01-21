e700dff29f006fcbf0210c1079cce1e27483141b
karoldavid <k.zysk@zoho.com>
2018-01-21 13:43:57 +0100

    feat: open alert box telling that local storage has been cleared and navigate back to decks screen on ok button press

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,12 +1,30 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
+import { Alert, AsyncStorage, View } from "react-native";
+import { Button } from "react-native-elements";
 import { styles } from "../utils/styles";
 
 class SettingsScreen extends Component {
+	clearAsyncStorage = async () => {
+		await AsyncStorage.clear();
+		Alert.alert("", "Local Storage Cleared", [
+			{
+				text: "OK",
+				onPress: () => this.props.navigation.navigate("decks")
+			}
+		]);
+	}
+
 	render() {
 		return (
 			<View style={styles.container}>
-				<Text>SettingsScreen</Text>
+				<Button
+					containerViewStyle={styles.buttonContainer}
+					buttonStyle={styles.button}
+					title="Clear Local Storage"
+					large
+					raised
+					onPress={this.clearAsyncStorage}
+				/>
 			</View>
 		);
 	}
