d511e1b188927fa621671db50b217761331f5151
karoldavid <k.zysk@zoho.com>
2018-01-21 13:00:58 +0100

    feat: save visited to AsyncStorage and navigate user to decks screen from second visit on

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -6,6 +6,20 @@ import { SLIDE_DATA } from "../utils/consts";
 import Slides from "../components/Slides";
 
 class WelcomeScreen extends Component {
+	componentWillMount() {
+		this._goToWelcomeScreen();
+	}
+
+	async _goToWelcomeScreen() {
+		let visited = await AsyncStorage.getItem("visited");
+
+		if (visited) {
+			this.props.navigation.navigate("decks");
+		} else {
+			AsyncStorage.setItem("visited", JSON.stringify({ visited: true }));
+		}
+	}
+
 	onSlidesComplete = () => {
 		this.props.navigation.navigate("decks");
 	};
