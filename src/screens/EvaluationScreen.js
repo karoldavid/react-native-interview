199fd20eb0e310d4a979362ea4ca1e89dc47d191
karoldavid <k.zysk@zoho.com>
2018-01-23 12:49:22 +0100

    feat: remove back navigation button from evaluation screen header

--- src/screens/EvaluationScreen.js
+++ src/screens/EvaluationScreen.js
@@ -3,6 +3,17 @@ import { Text, View } from "react-native";
 import { styles } from "../utils/styles";
 
 class EvaluationScreen extends Component {
+	static navigationOptions = ({ navigation }) => {
+		return {
+			title: "Evaluation",
+			visible: true,
+			headerTintColor: "white",
+			headerStyle: {
+				backgroundColor: "blue"
+			},
+			headerLeft: null
+		};
+	};
 	render() {
 		return (
 			<View style={styles.container}>
