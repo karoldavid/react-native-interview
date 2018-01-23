e85e8e41de1e9f62b219af47811ffd10da83a77f
karoldavid <k.zysk@zoho.com>
2018-01-23 17:51:11 +0100

    feat: show answer below question card title in evaluation screen on show answer button press

--- src/components/common/QuizListItem.js
+++ src/components/common/QuizListItem.js
@@ -4,21 +4,30 @@ import { Button, Card } from "react-native-elements";
 import { styles } from "../../utils/styles";
 
 export default class QuizListItem extends PureComponent {
+	state = {
+		show: false
+	};
+
+	toggleShowAnswer = () => {
+		this.setState({
+			show: !this.state.show
+		});
+	};
+
 	render() {
-		const { question, id } = this.props.item;
+		const { question, answer, id } = this.props.item;
 
 		return (
 			<Card key={id} title={question}>
-				<Button />
-				<View style={{ marginTop: 20, marginBottom: 20 }}>
-					<Text style={{ textAlign: "center" }}>
-						Know the Answer?
-					</Text>
-				</View>
-				<View style={styles.detailWrapper}>
-					<Text>No, swipe left.</Text>
-					<Text>Yes, swipe right.</Text>
-				</View>
+				<Button
+					title={this.state.show ? "Hide Answer" : "Show Answer"}
+					onPress={this.toggleShowAnswer}
+				/>
+				{this.state.show && (
+					<View style={{ marginTop: 20 }}>
+						<Text style={{ textAlign: "center" }}>{answer}</Text>
+					</View>
+				)}
 			</Card>
 		);
 	}
