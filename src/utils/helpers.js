5da45cafa5e24446908b693fcf1fff6752d7ed20
karoldavid <k.zysk@zoho.com>
2018-01-28 10:52:31 +0100

    feat: validate add new question form input

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,14 +1,33 @@
+import { isWebUri } from 'valid-url';
+
 export const validate = (data, inputs) => {
 	const errors = {};
 
 	inputs.map(input => {
 		const { propName, required } = input;
 		const value = data[propName];
-		
+
+		if (propName === "title" && value && value.length < 3) {
+			errors[propName] =
+				"Can not be shorter than three characters";
+		}
+
+		if (propName === "question" && value && value.length < 10) {
+			errors[propName] =
+				"Can not be shorter than ten characters";
+		}
+
+
+		if (propName === "answer" && value && value.length < 10) {
+			errors[propName] =
+				"Can not be shorter than ten characters";
+		}
+
 		if (propName === "title" && value && value.length < 3) {
 			errors[propName] =
 				"Can not be shorter than three and longer than 30 characters";
 		}
+
 		if (propName === "imageUrl" && value) {
 			const isValidImageUrl = /^http:|https:\/\/.+\.(gif|png|jpg|jpeg)$/i.test(
 				value
@@ -17,6 +36,10 @@ export const validate = (data, inputs) => {
 			if (!isValidImageUrl) {
 				errors[propName] = "Please enter a valid image url";
 			}
+		}
+
+		if (propName === "source" && value && !isWebUri(value) {
+				errors[propName] = "Please enter a valid url"
 		}
 	});
 
